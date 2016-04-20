deaaa5a62421378acde4cf2b3d6a688dc7d147f7
Kyle Kellogg <kyle@kylekellogg.com>
2016-04-20 12:31:29 -0400

    Updates to allow for appropriate state updating
    Addresses RPRO-1531
    
    On connection close, connection timeout, connection error, or disconnected the UI should now reflect that the streaming has stopped.
    On disconnected a toast message will also pop up notifying the user that they've been disconnected.

--- Red5ProiOS/PublishViewController.m
+++ Red5ProiOS/PublishViewController.m
@@ -10,6 +10,8 @@
 #import "PublishViewController.h"
 #import "ALToastView.h"
 
+#import "StreamViewController.h"
+
 @interface PublishViewController() {
     R5Stream *stream;
     BOOL isTogglable;
@@ -37,7 +39,41 @@
 }
 
 -(void)onR5StreamStatus:(R5Stream *)stream withStatus:(int)statusCode withMessage:(NSString *)msg {
-    [ALToastView toastInView:self.view withText:[NSString stringWithFormat:@"Stream: %s - %@", r5_string_for_status(statusCode), msg]];
+    if (msg && ![msg isEqualToString:@"null"]) {
+        [ALToastView toastInView:self.view withText:[NSString stringWithFormat:@"Stream: %s - %@", r5_string_for_status(statusCode), msg]];
+    } else {
+        [ALToastView toastInView:self.view withText:[NSString stringWithFormat:@"Stream: %s", r5_string_for_status(statusCode)]];
+    }
+    
+    switch (statusCode) {
+        case r5_status_connected:
+        case r5_status_netstatus:
+        case r5_status_start_streaming:
+            break;
+        case r5_status_connection_close:
+        case r5_status_connection_timeout:
+        case r5_status_connection_error:
+        case r5_status_disconnected: {
+            if (statusCode == r5_status_disconnected) {
+                [ALToastView toastInView:self.view withText:@"Your stream was disconnected"];
+            }
+            
+            [self stop:YES];
+            
+            if (self.streamViewController) {
+                [self.streamViewController onCameraTouch:nil];
+            } else {
+                NSLog(@"No StreamViewController reference!");
+            }
+            
+            break;
+        }
+        case r5_status_stop_streaming:
+            [self stop:NO];
+            break;
+        default:
+            break;
+    }
 }
 
 -(void)establishPreview {
